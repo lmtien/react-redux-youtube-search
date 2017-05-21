856c66541c7dfb92beb0c4afdd75e1a7bf588932
Tien Le <tiduslmt@gmail.com>
2017-05-22 01:30:21 +0800

    video-list selection

--- src/index.js
+++ src/index.js
@@ -12,10 +12,16 @@ class App extends Component {
   constructor(props) {
     super(props);
     
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
     
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      });
     });
   }
   
@@ -23,8 +29,11 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
+          videos={this.state.videos}
+        />
       </div>
     );
   }

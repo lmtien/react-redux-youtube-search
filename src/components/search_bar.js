d0e21052cf9a67b34f054370752509223c60addd
Tien Le <tiduslmt@gmail.com>
2017-05-22 00:44:57 +0800

    adding video list and detail

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -12,7 +12,7 @@ class SearchBar extends Component {
       <div>
         <input
           value={this.state.term}
-          onChange={event => this.setState({ term: event.target.value })} />
+          onChange={(event) => this.setState({ term: event.target.value })} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCxaLfV4agBhp6YWKhregGSgaOfmFTO2pg';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = { videos: [] };
+    
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));

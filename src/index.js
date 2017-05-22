f6ff703f58a6ae46ebac55dac186e6179824a8bd
Tien Le <tiduslmt@gmail.com>
2017-05-22 11:01:28 +0800

    completed features

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -17,7 +18,11 @@ class App extends Component {
       selectedVideo: null
     };
     
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.videoSearch('surfboards');
+  }
+  
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term}, videos => {
       this.setState({
         videos,
         selectedVideo: videos[0]
@@ -26,12 +31,14 @@ class App extends Component {
   }
   
   render() {
+    const videoSearch = _.debounce(term => {this.videoSearch(term)}, 300);
+    
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
-          onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
           videos={this.state.videos}
         />
       </div>
